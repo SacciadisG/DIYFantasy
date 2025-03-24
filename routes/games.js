e2b72ef3b4eaa93b0a775e04e4f48ae15efd64ce
Greg Sacciadis <greg.sacciadis@gmail.com>
2025-03-23 21:37:35 -0400

    Fixed MergeParams bug with Express Router

--- routes/games.js
+++ routes/games.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 const Player = require('../models/player.js');
 const Game = require('../models/game.js');
 
