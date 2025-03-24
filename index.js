3a240efe5900b40b6f9201f69e8ce3c7f29e76c5
Greg Sacciadis <greg.sacciadis@gmail.com>
2025-03-23 21:22:40 -0400

    Updated routes for games

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Game = require('./models/game');
 const app = express(); //Easier to write "app". [method]
 
 const players = require('./routes/players');
+const games = require('./routes/games');
 
 //Connect to mongoose
 mongoose.connect('mongodb://localhost:27017/fantasy');
@@ -31,53 +32,12 @@ app.use(methodOverride('_method'));
 
 //Routed Pages here
 app.use('/players', players)
+app.use('/players/:id/games', games)
 
 //HOME PAGE
 app.get('/', (req, res) => {
     res.render('home')
 });
-
-//Create a New Game
-app.get('/players/:id/games', async (req, res) => {
-    const player = await Player.findById(req.params.id)
-    res.render('games/new', {player});
-})
-
-//Add a new game
-app.post('/players/:id/games', async (req, res) => {
-    const player = await Player.findById(req.params.id); //Find the player
-    const game = new Game(req.body.game); //Create a Game with the input stats
-    player.games.push(game);
-    await game.save();
-    await player.save();
-    res.redirect(`/players/${player._id}`); //Backticks help create a template literal, so you concat the string "/players/ + the player id"
-})
-
-//Edit a game
-app.get('/players/:id/games/:gameId/edit', async (req, res) => {
-    const player = await Player.findById(req.params.id);
-    const game = await Game.findById(req.params.gameId);
-    res.render('games/edit', {player, game})
-})
-
-app.put('/players/:id/games/:gameId', async(req, res) => {
-    const { id, gameId } = req.params; 
-    const player = await Player.findById(id);
-    const game = await Game.findByIdAndUpdate(gameId, {...req.body.game});
-    await game.save();
-    await player.save();
-    //Remember that the "..." is the spread operator and splits the req body into multiple objects (i.e. our player values)
-    res.redirect(`/players/${id}`) //Go back to that player's page
-})
-
-//Delete a game
-app.delete('/players/:id/games/:gameId', async (req, res) => {
-    const { id, gameId } = req.params;
-    const player = await Player.findById(id);
-    await Game.findByIdAndDelete(gameId);
-    await player.save();
-    res.redirect(`/players/${id}`) //Go back to that player's page
-})
 
 //Runs server on port 3000
 app.listen(3000, () => {
---
+++ routes/games.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const Player = require('../models/player.js');
+const Game = require('../models/game.js');
+
+//Create a New Game
+router.get('/', async (req, res) => {
+    const player = await Player.findById(req.params.id)
+    res.render('games/new', {player});
+})
+
+//Add a new game
+router.post('/', async (req, res) => {
+    const player = await Player.findById(req.params.id); //Find the player
+    const game = new Game(req.body.game); //Create a Game with the input stats
+    player.games.push(game);
+    await game.save();
+    await player.save();
+    res.redirect(`/players/${player._id}`); //Backticks help create a template literal, so you concat the string "/players/ + the player id"
+})
+
+//Edit a game
+router.get('/:gameId/edit', async (req, res) => {
+    const player = await Player.findById(req.params.id);
+    const game = await Game.findById(req.params.gameId);
+    res.render('games/edit', {player, game})
+})
+
+router.put('/:gameId', async(req, res) => {
+    const { id, gameId } = req.params; 
+    const player = await Player.findById(id);
+    const game = await Game.findByIdAndUpdate(gameId, {...req.body.game});
+    await game.save();
+    await player.save();
+    //Remember that the "..." is the spread operator and splits the req body into multiple objects (i.e. our player values)
+    res.redirect(`/players/${id}`) //Go back to that player's page
+})
+
+//Delete a game
+router.delete('/:gameId', async (req, res) => {
+    const { id, gameId } = req.params;
+    const player = await Player.findById(id);
+    await Game.findByIdAndDelete(gameId);
+    await player.save();
+    res.redirect(`/players/${id}`) //Go back to that player's page
+})
+
+module.exports = router;
